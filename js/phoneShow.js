015f9a6d641668138e4fd148d583a2c7c0f868df
Marija Vuković <marijavukovic.tmf@gmail.com>
2018-11-13 01:11:21 +0100

    Final changes

---
+++ js/phoneShow.js
@@ -0,0 +1,44 @@
+// Scan to call
+$('.qrCode').mouseover(function() {
+  $('.phone').css("visibility","visible");
+});
+
+$('.qrCode').mouseout(function() {
+  $('.phone').css("visibility","hidden");
+});
+
+// CV
+$('.fa-file').mouseover(function() {
+  $('.cv').css("visibility","visible");
+});
+
+$('.fa-file').mouseout(function() {
+  $('.cv').css("visibility","hidden");
+});
+
+// Mail
+$('.fa-envelope').mouseover(function() {
+  $('.mail').css("visibility","visible");
+});
+
+$('.fa-envelope').mouseout(function() {
+  $('.mail').css("visibility","hidden");
+});
+
+// git
+$('.fa-github').mouseover(function() {
+  $('.git').css("visibility","visible");
+});
+
+$('.fa-github').mouseout(function() {
+  $('.git').css("visibility","hidden");
+});
+
+// ln
+$('.fa-linkedin').mouseover(function() {
+  $('.ln').css("visibility","visible");
+});
+
+$('.fa-linkedin').mouseout(function() {
+  $('.ln').css("visibility","hidden");
+});
